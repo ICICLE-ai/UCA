afed5295c7fc9fe709e2d6ff0d19bc0fd01e0717
ncliffel <85183849+ncliffel@users.noreply.github.com>
2025-06-11 10:48:13 -0400

    creating general strcuture for smart scheduler

---
+++ src/smart_scheduler/model_inference_scheduler_unit_test.py
@@ -0,0 +1,6 @@
+import unittest
+from src.smart_scheduler.model_inference_scheduler import ModelInferenceScheduler
+
+class TestModelInferenceScheduler(unittest.TestCase):
+	def test_validate(self):
+		raise NotImplementedError("test_validate not implemented yet")
---
+++ src/smart_scheduler/model_training_scheduler.py
@@ -0,0 +1,31 @@
+from src.model_commons.patra.validator import Validator
+from src.imt_component import IMTComponent
+from src.smart_scheduler.smart_scheduler import SmartScheduler
+
+class ModelTrainingScheduler(SmartScheduler):
+	def __init__(self, inputs:dict):
+		self.inputs = inputs
+
+	def ValidateInputs(self):
+		raise NotImplementedError("ValidateInputs function has yet to be implemented")
+
+	def Estimate(self):
+		raise NotImplementedError("Estimate function has yet to be implemented")
+
+	def SelectBestSystem(self):
+		raise NotImplementedError("SelectBestSystem has yet to be implemented")
+
+	def MonitorService(self):
+		raise NotImplementedError("MonitorService has yet to be implemented")
+
+	def StopService(self):
+		raise NotImplementedError("StopService has yet to be implemented")
+
+	def Start(self):
+		raise NotImplementedError("Start has yet to be implemented")
+
+	def Terminate(self):
+		raise NotImplementedError("Terminate has yet to be implemented")
+
+	def Restart(self):
+		raise NotImplementedError("Restart has yet to be implemented")
