ef781ca779e1fe62f210ebcb6e50ca1e1eabba78
ncliffel <85183849+ncliffel@users.noreply.github.com>
2025-06-19 19:40:39 -0400

    added the ability to throw custom errors when a type is wrong

--- src/model_commons/patra/validator.py
+++ src/model_commons/patra/validator.py
@@ -6,7 +6,7 @@ from patra_toolkit.patra_model_card import Metric
 
 class Validator():
 	@staticmethod
-	def Validate(var, expec:str):
+	def Validate(var, expec:str, error_type=None):
 		# types checked for
 		types = ["ModelCard","BiasAnalysis","AIModel","ExplainabilityAnalysis","Metric",
 			"str","int","list","dict","list[str]","number","list[int]"]
@@ -16,42 +16,49 @@ class Validator():
 		# validating	
 		if expec == "ModelCard":
 			if not isinstance(var, ModelCard):
-				raise TypeError(Validator.ErrorMessage(var, expec))
+				Validator.ThrowTypeError(var, expec, error_type)
 		if expec == "BiasAnalysis":
 			if not isinstance(var, BiasAnalysis):
-				raise TypeError(Validator.ErrorMessage(var, expec))
+				Validator.ThrowTypeError(var, expec, error_type)
 		if expec == "AIModel":
 			if not isinstance(var, AIModel):
-				raise TypeError(Validator.ErrorMessage(var, expec))
+				Validator.ThrowTypeError(var, expec, error_type)
 		if expec == "ExplainabilityAnalysis":
 			if not isinstance(var, ExplainabilityAnalysis):
-				raise TypeError(Validator.ErrorMessage(var, expec))
+				Validator.ThrowTypeError(var, expec, error_type)
 		if expec == "Metric":
 			if not isinstance(var, Metric):
-				raise TypeError(Validator.ErrorMessage(var, expec))
+				Validator.ThrowTypeError(var, expec, error_type)
 		if expec == "str":
 			if not isinstance(var, str):
-				raise TypeError(Validator.ErrorMessage(var, expec))
+				Validator.ThrowTypeError(var, expec, error_type)
 		if expec == "list":
 			if not isinstance(var, list):
-				raise TypeError(Validator.ErrorMessage(var, expec))
+				Validator.ThrowTypeError(var, expec, error_type)
 		if expec == "int":
 			if not isinstance(var, int):
-				raise TypeError(Validator.ErrorMessage(var, expec))
+				Validator.ThrowTypeError(var, expec, error_type)
 		if expec == "number":
 			if not isinstance(var, int) and not isinstance(var, float):
-				raise TypeError(Validator.ErrorMessage(var, expec))
+				Validator.ThrowTypeError(var, expec, error_type)
 		if expec == "dict":
 			if not isinstance(var, dict):
-				raise TypeError(Validator.ErrorMessage(var, expec))
+				Validator.ThrowTypeError(var, expec, error_type)
 		if expec == "list[str]":
-			Validator.Validate(var, "list")
+			Validator.Validate(var, "list", error_type)
 			for val in var:
-				Validator.Validate(val, "str")
+				Validator.Validate(val, "str", error_type)
 		if expec == "list[int]":
-			Validator.Validate(var, "list")
+			Validator.Validate(var, "list", error_type)
 			for val in var:
-				Validator.Validate(val, "int")
+				Validator.Validate(val, "int", error_type)
+
+	@staticmethod
+	def ThrowTypeError(var, expec, error_type=None):
+		error_message = Validator.ErrorMessage(var, expec)
+		if error_type is not None:
+			raise error_type(error_message)
+		raise TypeError(error_message)
 
 	@staticmethod
 	def ErrorMessage(var, expec:str) -> str:
