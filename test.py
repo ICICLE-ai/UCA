b3c889bda3a0da090f182ef9c88d73fa4861e219
ncliffel <85183849+ncliffel@users.noreply.github.com>
2025-06-25 12:42:46 -0400

    initial commits for hugging face coding + test cases

--- test.py
+++ test.py
@@ -4,24 +4,25 @@ from src.model_commons.patra.validator_unit_test import TestValidatorClass
 from src.model_commons.patra.ai_model_wrapper_unit_test import TestAIModelWrapperClass
 from src.model_commons.patra.model_card_wrapper_unit_test import TestModelCardWrapperClass
 from src.model_commons.ml_hub.ml_hub_caller_unit_test import TestMLHubCallerClass
+from src.model_commons.hugging_face.hugging_unit_test import TestHuggingClass
 
 if __name__ == "__main__":
 	# auto resetting colorama color
 	init(autoreset=True)
 
-	# testing the Validator class in the src/patra/validator.py file	
+	# testing the Validator class in the src/model_commons/patra/validator.py file	
 	print(Fore.MAGENTA + Style.BRIGHT + "running test for patra Validator class")
 	validatorTestSuite = unittest.TestLoader().loadTestsFromTestCase(TestValidatorClass)
 	unittest.TextTestRunner(verbosity=2).run(validatorTestSuite)
 	print()
 	
-	# testing the AIModelWrapper class in the src/patra/ai_model_wrapper.py file
+	# testing the AIModelWrapper class in the src/model_commons/patra/ai_model_wrapper.py file
 	print(Fore.CYAN + Style.BRIGHT + "running test for patra ai model wrapper class")
 	aiModelTestSuite = unittest.TestLoader().loadTestsFromTestCase(TestAIModelWrapperClass)
 	unittest.TextTestRunner(verbosity=2).run(aiModelTestSuite)
 	print()
 
-	# testing the ModelCardWrapper class in the src/patra/model_card_wrapper.py file
+	# testing the ModelCardWrapper class in the src/model_commons/patra/model_card_wrapper.py file
 	print(Fore.MAGENTA + Style.BRIGHT + "running test for model card wrapper class")
 	modelCardTestSuite = unittest.TestLoader().loadTestsFromTestCase(TestModelCardWrapperClass)
 	unittest.TextTestRunner(verbosity=2).run(modelCardTestSuite)
@@ -31,3 +32,9 @@ if __name__ == "__main__":
 	print(Fore.CYAN + Style.BRIGHT + "running test for ml hub caller class")
 	mlHubCallerTestSuite = unittest.TestLoader().loadTestsFromTestCase(TestMLHubCallerClass)
 	unittest.TextTestRunner(verbosity=2).run(mlHubCallerTestSuite)
+	print()
+
+	# testing the Hugging calss in the src/model_commons/hugging_face/hugging.py file
+	print(Fore.CYAN + Style.BRIGHT + "running test for hugging class")
+	huggingTestSuite = unittest.TestLoader().loadTestsFromTestCase(TestHuggingClass)
+	unittest.TextTestRunner(verbosity=2).run(huggingTestSuite)
